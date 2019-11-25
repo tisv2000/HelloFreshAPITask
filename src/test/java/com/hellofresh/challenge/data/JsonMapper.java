223efaf137e49dffa9d6d888f7de230f06ddd7e4
Lana Timofeeva <tisv2000@google.com>
2019-11-25 08:07:44 +0100

    Do final refactoring

--- src/test/java/com/hellofresh/challenge/data/JsonMapper.java
+++ src/test/java/com/hellofresh/challenge/data/JsonMapper.java
@@ -2,8 +2,19 @@ package com.hellofresh.challenge.data;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hellofresh.challenge.models.Response;
 
 public class JsonMapper {
+
+    public static <T> Response<T> parseResponse(io.restassured.response.Response response,
+                                                 int expectedStatus,
+                                                 Class<T> clazz) {
+        T gottenObject = null;
+        if (response.statusCode() == expectedStatus) {
+            gottenObject = readObjectFromResponseBody(response, clazz);
+        }
+        return new Response<T>(response.statusCode(), gottenObject);
+    }
 
     public static <T> T readObjectFromResponseBody(io.restassured.response.Response response, Class<T> clazz) {
         return readObjectFromJson(response.body().asString(), clazz);
--- src/test/java/com/hellofresh/challenge/service/BookingRestAPI.java
+++ src/test/java/com/hellofresh/challenge/service/BookingRestAPI.java
@@ -6,8 +6,7 @@ import com.hellofresh.challenge.models.CreatedBooking;
 import com.hellofresh.challenge.models.Response;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.hellofresh.challenge.data.JsonMapper.readObjectFromResponseBody;
-import static com.hellofresh.challenge.data.JsonMapper.writeObjectToJson;
+import static com.hellofresh.challenge.data.JsonMapper.*;
 import static io.restassured.RestAssured.given;
 
 @Slf4j
@@ -20,12 +19,11 @@ public class BookingRestAPI {
         io.restassured.response.Response response =
                 given()
                         .pathParam("id", bookingId)
-                        .contentType("application/json")
                         .when()
                         .get(BASE_ENDPOINT_WITH_BOOKING_ID);
         log.info("GOTTEN BOOKING BY ID RESPONSE {}", response.asString());
 
-        return getObjectFromResponseBody(response, response.statusCode(), Booking.class);
+        return parseResponse(response, response.statusCode(), Booking.class);
     }
 
     public static Response<BookingList> getBookingsByRoomId(int roomId) {
@@ -36,19 +34,18 @@ public class BookingRestAPI {
                         .get(BASE_URL);
         log.info("GOTTEN BOOKING BY ROOM ID RESPONSE {}", response.asString());
 
-        return getObjectFromResponseBody(response, response.statusCode(), BookingList.class);
+        return parseResponse(response, response.statusCode(), BookingList.class);
     }
 
     public static Response<BookingList> getBookings() {
         io.restassured.response.Response response =
                 given()
-                        .contentType("application/json")
                         .when()
                         .get(BASE_URL);
         log.info("GOTTEN BOOKINGS RESPONSE {}", response.asString());
         log.info("GOTTEN STATUS {}", response.getStatusCode());
 
-        return getObjectFromResponseBody(response, response.statusCode(), BookingList.class);
+        return parseResponse(response, response.statusCode(), BookingList.class);
     }
 
     public static Response<CreatedBooking> createBooking(Booking bookingToCreate) {
@@ -63,24 +60,6 @@ public class BookingRestAPI {
         log.info("CREATING BOOKING RESPONSE {}", response.body().asString());
         log.info("CREATED BOOKING STATUS {}", response.getStatusCode());
 
-        return getObjectFromResponseBody(response, response.statusCode(), CreatedBooking.class);
-    }
-
-    private static <T> Response<T> getObjectFromResponseBody(io.restassured.response.Response response,
-                                                             int expectedStatus,
-                                                             Class<T> clazz) {
-        T gottenObject = null;
-        try {
-            gottenObject = clazz.newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        if (response.statusCode() == expectedStatus) {
-            gottenObject = readObjectFromResponseBody(response, clazz);
-        }
-        return new Response<T>(response.statusCode(), gottenObject);
+        return parseResponse(response, response.statusCode(), CreatedBooking.class);
     }
 }
